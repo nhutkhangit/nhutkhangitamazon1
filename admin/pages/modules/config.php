bc3e13be0bb7bcaa07aae1a021c6bcb9bded9369
kendy <kendy.lien@beyondedge.co>
2021-01-07 11:59:14 +0700

    remove comment fb

--- admin/pages/modules/config.php
+++ admin/pages/modules/config.php
@@ -1,5 +1,4 @@
 <?php 
-	
 	// connect db
 	$conn = mysqli_connect("database-2.cx3zh21e5jvo.ap-southeast-1.rds.amazonaws.com","nhutkhangitweb","5lDFj9u7oE3SCt7iA8VM","nhutkhangitweb");
 	date_default_timezone_set('Asia/Bangkok');
